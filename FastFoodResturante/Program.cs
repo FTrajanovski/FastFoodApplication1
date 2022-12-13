6784e28a09e9fb9190d844baf8b3bb01683c72d1
Filip <filip.trajanovski@student.nbi-handelsakademin.se>
2022-12-13 14:10:23 +0100

    .

--- FastFoodResturante/Program.cs
+++ FastFoodResturante/Program.cs
@@ -13,7 +13,7 @@ namespace FastFoodResturante
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frm_item());
+            Application.Run(new Inloggning());
         }
 
        
